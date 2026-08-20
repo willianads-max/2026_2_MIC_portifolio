ebb71c1ad3925ba9d453ff20423137187fde2185
Willian <willian.ads@aluno.ifsc.edu.br>
2026-08-20 11:36:55 -0300

    Simulação Led com Chave
    Signed-off-by: Willian <willian.ads@aluno.ifsc.edu.br>

--- portifolio_mic/aula1_gpio/main.c
+++ portifolio_mic/aula1_gpio/main.c
@@ -10,11 +10,14 @@
 #include "util/delay.h"
 
 int main(void) {
-	DDRB |= 0b00100000;
+	DDRB |= 0b00100000; //define o pino PB5 como saida
+	DDRB &= 0b1110111; //define o pino PDB3 como entrada
 	while(1) {
+		if((PIND & 0b00001000)!=0) { //lER VALOR DA CHAVE PD3
 		PORTB |= 0b00100000; //Atribui nível lógico alto ao pino PB5
 		_delay_ms(100);
 		PORTB &= 0b11011111; // Atribui nível lógico baixo ao pino PB5
 		_delay_ms(900);
+		}
 	}
 }
