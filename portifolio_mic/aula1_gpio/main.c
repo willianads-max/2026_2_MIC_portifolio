ccd2ee523fbbb7ee7fd0441a22ddc8af4d455c92
Willian <willian.ads@aluno.ifsc.edu.br>
2026-08-20 09:27:11 -0300

    Esqueleto da solução da aula1
    Signed-off-by: Willian <willian.ads@aluno.ifsc.edu.br>

---
+++ portifolio_mic/aula1_gpio/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 8/20/2026 8:58:56 AM
+ *  Author: Aluno
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
