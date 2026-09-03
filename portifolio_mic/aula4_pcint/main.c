0fa6b389daf8430713c6edb3c00e9238dd2b5ba3
willianads-max <willian.ads@aluno.ifsc.edu.br>
2026-09-03 10:49:39 -0300

    Firmware com PCINT leds

--- portifolio_mic/aula4_pcint/main.c
+++ portifolio_mic/aula4_pcint/main.c
@@ -1,37 +1,42 @@
-/*
- * main.c
- *
- * Created: 9/3/2026 8:57:08 AM
- *  Author: Aluno
- */ 
-
+#define F_CPU 16000000
 #include <xc.h>
+#include "avr/interrupt.h"
+#include "util/delay.h"
 
-ISR(PCINT0_vect) {
-	
-	
-	
-}
+
 void GPIO_config() {
 	/*0b0000 1111; inicialmente b0,b1,b2,b3 = 1. Se coloca o "~" elesinvertem e mudam pra zero */
-	DDRB &= ~((1<<DDB0))|((1<<DDB1))|((1<<DDB2))|((1<<DDB3)); // Portas PB0, PB1, PB2, PB3 como entrada
-	DDRD = 0xff; //seta todo mundo como saída, mesmo que 0b11111111
-	DDRD |= (1<<DDB0))|((1<<DDB1))|((1<<DDB2))|((1<<DDB3)
-		 | ((1<<DDB4))|((1<<DDB5))|((1<<DDB6))|((1<<DDB7); // configura PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7 COMO SAÍDA
+	DDRB &= ~((1<<DDB0)|(1<<DDB1)|(1<<DDB2)|(1<<DDB3)); // Portas PB0, PB1, PB2, PB3 como entrada
+	PORTB |= (1<<DDB0)|(1<<DDB1)|(1<<DDB2)|(1<<DDB3); //Habilita o pullup nos pinos PB0, PB1, PB2, PB3
+	//DDRD = 0xff; seta todo mundo como saída, mesmo que 0b11111111
+	DDRD |= (1<<DDD0)|(1<<DDD1)|(1<<DDD2)|(1<<DDD3)
+		 | (1<<DDD4)|(1<<DDD5)|(1<<DDD6)|(1<<DDD7); // configura PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7 COMO SAÍDA*/
+	DDRC |= (1<<DDC0); // Pino PC0  como saída
+}
+
+void GPIO_incBar(){
+	PORTD = PORTD >> 1;
+	PORTD |= 0b10000000; //Aciona bit mais significativo
 }
 
 void PCINT_config() {
-	
-	
+	PCICR   |= (1<<PCIE0); // Habilita grupo PCINT0 - PCINT7
+	PCMSK0 |= (1<<PCINT3)|(1<<PCINT2)|(1<<PCINT1)|(1<<PCINT0); //Habilita interrupção PCINT0-PCINT3
 	
 }
 
+ISR(PCINT0_vect) {
+	PORTC |= (1<<PORTC0);
+	_delay_ms(100);
+	PORTC &= ~(1<<PORTC0);
+	GPIO_incBar();
+}
 
 int main(void)
 {
-    while(1)
-    {
-		
-		
+    GPIO_config();
+	PCINT_config();
+	sei();
+	while(1){
     }
 }
