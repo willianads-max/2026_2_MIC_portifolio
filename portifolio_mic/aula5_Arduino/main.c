a083da0e4439a29fc24f6bda92c411b056ccc0c6
willianads-max <willian.ads@aluno.ifsc.edu.br>
2026-09-10 10:45:41 -0300

    Codigo parte 2 da aula 5 com arduino

--- portifolio_mic/aula5_Arduino/main.c
+++ portifolio_mic/aula5_Arduino/main.c
@@ -1,5 +1,5 @@
 
-#define F_CPU 16000000
+/* #define F_CPU 16000000
 #include <xc.h>
 #include "util/delay.h"
 
@@ -12,4 +12,21 @@ int main (void) {
 	 while (1) {
 		 PORTB ^= (1<<PORTB5) ; // Alterna o estado do LED
 		 _delay_ms (500) ;
-	 }}
+	 }} */#define F_CPU 16000000#include <xc.h>#include "util/delay.h"void GPIO_incBar () {
+	PORTD = PORTD >> 1;
+	PORTD |= 0b10000000;
+}
+
+void GPIO_decBar () {
+	PORTD = PORTD << 1;}void GPIO_config () {
+	DDRD = 0xFF; // pinos usados pelos LEDs como saida
+	PORTD = 0x00; // parte com a barra apagada
+}
+
+int main (void) {
+	GPIO_config () ;
+	while (1) {
+		for (uint8_t i = 0; i < 8; i ++) {GPIO_incBar () ; _delay_ms (200);}
+		for (uint8_t i = 0; i < 8; i ++) {GPIO_decBar () ; _delay_ms (200);}
+	}}
+	
